d9a3e98a421129f3271f39ef53419c7b0cc7c6a7
@adityakadlag2004 <adityakadlag2004@gmail.com>
2021-02-06 15:42:55 +0530

    IntroActivity.kt designing done

--- app/src/main/java/com/android/mvvmdatabind2/activities/auth/RegisterActivity.kt
+++ app/src/main/java/com/android/mvvmdatabind2/activities/auth/RegisterActivity.kt
@@ -9,54 +9,58 @@ import androidx.lifecycle.ViewModelProviders
 import com.android.mvvmdatabind2.R
 import com.android.mvvmdatabind2.activities.MainActivity
 import com.android.mvvmdatabind2.databinding.ActivityRegisterBinding
+import com.android.mvvmdatabind2.di.components.DaggerFactoryComponent
+import com.android.mvvmdatabind2.di.modules.FactoryModule
+import com.android.mvvmdatabind2.di.modules.RepositoryModule
 import com.android.mvvmdatabind2.mvvm.repository.AuthRepository
 import com.android.mvvmdatabind2.mvvm.viewmodels.AuthViewModel
-import com.android.mvvmdatabind2.mvvm.factory.ModelFactory
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var authRepository: AuthRepository
     private lateinit var viewModel: AuthViewModel
-    private lateinit var factory: ModelFactory
     private lateinit var mAuth: FirebaseAuth
     private val TAG = "RegisterActivity"
-    private var verifiedboolean=false
+    private lateinit var component: DaggerFactoryComponent
+    private var verifiedboolean = false
     private var currentuser: FirebaseUser? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         mAuth = FirebaseAuth.getInstance()
 
-        authRepository= AuthRepository(this)
-        factory= ModelFactory(authRepository)
-        viewModel= ViewModelProviders.of(this,factory).get(AuthViewModel::class.java)
 
-        val binding=DataBindingUtil
-            .setContentView<ActivityRegisterBinding>(this,R.layout.activity_register)
+        component = DaggerFactoryComponent.builder()
+            .factoryModule(FactoryModule(AuthRepository(this)))
+            .repositoryModule(RepositoryModule(this))
+            .build() as DaggerFactoryComponent
+        viewModel =
+            ViewModelProviders.of(this, component.getFactory()).get(AuthViewModel::class.java)
+        val binding = DataBindingUtil
+            .setContentView<ActivityRegisterBinding>(this, R.layout.activity_register)
             .apply {
                 this.lifecycleOwner = this@RegisterActivity
-                this.viewmodel=viewModel
+                this.viewmodel = viewModel
             }
 
         binding.txtReg.setOnClickListener {
             Intent(this, LoginActivity::class.java).also { startActivity(it) }
         }
     }
+
     override fun onStart() {
         super.onStart()
         mAuth = FirebaseAuth.getInstance()
         currentuser = mAuth.currentUser
-        if (currentuser!=null) {
-            verifiedboolean=currentuser!!.isEmailVerified
+        if (currentuser != null) {
+            verifiedboolean = currentuser!!.isEmailVerified
             if (verifiedboolean) {
                 Intent(this, MainActivity::class.java).also {
                     it.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(it)
                 }
             }
-        }
-        else{
+        } else {
             Log.d(TAG, "onStart:Not Verified ")
         }
     }
