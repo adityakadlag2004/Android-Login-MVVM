9288d4d4446c1a20e7bd554f21d97b023000f9c1
@adityakadlag2004 <adityakadlag2004@gmail.com>
2021-02-23 17:35:56 +0530

    mem details done

--- app/src/main/java/com/android/mvvmdatabind2/fragments/homepage/Home.kt
+++ app/src/main/java/com/android/mvvmdatabind2/fragments/homepage/Home.kt
@@ -5,23 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import com.android.mvvmdatabind2.R
-
-
+import com.android.mvvmdatabind2.di.components.DaggerFactoryComponent
+import com.android.mvvmdatabind2.di.modules.FactoryModule
+import com.android.mvvmdatabind2.di.modules.RepositoryModule
+import com.android.mvvmdatabind2.mvvm.repository.MainRepository
+import com.android.mvvmdatabind2.mvvm.repository.UserDataRepo
+import com.android.mvvmdatabind2.mvvm.viewmodels.MainViewModel
+import com.android.mvvmdatabind2.mvvm.viewmodels.UserDataViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 
 class Home : Fragment() {
-
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var viewModel: UserDataViewModel
+    private lateinit var component: DaggerFactoryComponent
+    private var currentuser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        mAuth = FirebaseAuth.getInstance()
+        currentuser = mAuth.currentUser
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        mAuth = FirebaseAuth.getInstance()
+        currentuser = mAuth.currentUser
+        component = DaggerFactoryComponent.builder()
+            .repositoryModule(RepositoryModule(view.context))
+            .factoryModule(FactoryModule(UserDataRepo(view.context)))
+            .build() as DaggerFactoryComponent
+        viewModel = ViewModelProviders.of(this, component.getFactory())
+            .get(UserDataViewModel::class.java)
+        return view
     }
 }
